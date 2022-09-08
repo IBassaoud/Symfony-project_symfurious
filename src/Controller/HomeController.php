1e4767d2a293d949d9d64b03a305bd6dee2ee823
IBassaoud <ismail.bassaoud@gmail.com>
2022-09-08 11:15:48 +0000

    Modification : style register + login invalidate message + managing access

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/", name="app_home")
      */
     public function index(): Response
     {
         // session_start();
         // dd(unserialize($_SESSION['_sf2_attributes']['_security_main']));
+        // dd($this->isGranted('ROLE_ADMIN'));
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,19 +10,19 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/", name="app_login")
+     * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
+        
+        if ($this->getUser()) {
+            return $this->redirectToRoute('target_path');
+        }
+        
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
